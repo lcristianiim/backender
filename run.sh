4ad96d5f80dcb47a7a2abe72f184c7072514ff3d
lcristianiim <lcristianiim@yahoo.com>
2024-06-10 01:26:53 +0300

    Clean shell scripts

--- run.sh
+++ run.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-#java -Dfile.encoding=UTF-8 -classpath "dependencies/jakarta.transaction-api-2.0.1.jar:dependencies/jboss-logging-3.5.0.Final.jar:dependencies/hibernate-commons-annotations-6.0.6.Final.jar:dependencies/classmate-1.5.1.jar:dependencies/byte-buddy-1.14.7.jar:dependencies/jakarta.xml.bind-api-4.0.0.jar" --module-path modules:dependencies -m com.backender.webservermodule/com.webserver.Application
-#java -Dfile.encoding=UTF-8 -classpath "dependencies/*" -p modules -m com.backender.webservermodule/com.webserver.Application
-
 java -p modules:dependencies -m com.backender.webservermodule/com.webserver.Application
