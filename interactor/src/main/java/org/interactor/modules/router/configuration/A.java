d5e5ee5b1145c3befe55ac056ea9ab22cac142d3
Cristian <lcristianiim@gmail.com>
2024-12-14 23:37:21 +0200

    Clean up test and pom.xml

--- interactor/src/main/java/org/interactor/modules/router/configuration/A.java
+++
@@ -1,7 +0,0 @@
-package org.interactor.modules.router.configuration;
-
-public class A {
-    public void mata() {
-        System.out.println("@@@ mata");
-    }
-}
--- router/src/test/java/org/backender/router/RouterImplementationTest.java
+++ router/src/test/java/org/backender/router/RouterImplementationTest.java
@@ -1,12 +1,12 @@
 package org.backender.router;
 
 import org.interactor.ApplicationConfiguration;
-import org.interactor.modules.router.configuration.A;
 import org.interactor.modules.router.dtos.Controller;
 import org.interactor.modules.router.dtos.ReqContextDTO;
 import org.interactor.modules.router.dtos.RouterResponse;
 import org.junit.jupiter.api.Test;
 
+import java.text.MessageFormat;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,9 +21,6 @@ class RouterImplementationTest {
         String requestPath = apiPrefix + "/cool-path";
         String body = "associated body";
         int responseCode = 200;
-
-        A mata = new A();
-        mata.mata();
 
         RouterImplementation routerImplementation = new RouterImplementation();
 
@@ -41,61 +38,61 @@ class RouterImplementationTest {
 
     }
 
-//    @Test
-//    void givenRegisteredPathWithRequestAndQueryParams_ShouldReturnResponseProcessedByController() {
-//        String apiPrefix = ApplicationConfiguration.INSTANCE.getApiPath();
-//        String requestPath = apiPrefix + "/products/1/something/blue?name=ball&age=20";
-//        String body = "1,blue,ball,20";
-//
-//        RouterImplementation routerImplementation = new RouterImplementation();
-//
-//        Map<String, Controller> getRoutes = new HashMap<>();
-//        getRoutes.put("products/{id}/something/{color}?name&age", getControllerWithPathAndQueryParams());
-//        routerImplementation.setGetRoutes(getRoutes);
-//
-//        ReqContextDTO ctx = new ReqContextDTO();
-//        ctx.setRequestPath(requestPath);
-//        RouterResponse result = routerImplementation.get(ctx);
-//
-//        assertEquals(body, result.getBody());
-//    }
-//
-//    Controller getControllerWithPathAndQueryParams() {
-//        return new Controller() {
-//            private String id;
-//            private String color;
-//            private String name;
-//            private String age;
-//
-//
-//            @Override
-//            public RouterResponse getResponse() {
-//                RouterResponse routerResponse = new RouterResponse();
-//                routerResponse.setType(JSON);
-//                routerResponse.setCode(200);
-//                routerResponse.setBody(MessageFormat.format("{0},{1},{2},{3}", id, color, name, age));
-//                return routerResponse;
-//            }
-//
-//            @Override
-//            public void initialize(ReqContextDTO controllerData, String registeredPath) {
-//                PathCommonOperations operations = new PathCommonOperations();
-//
-//                Map<String, String> pathParams = operations.getPathParams(
-//                        registeredPath, PathOperations.getPathWithoutTheAPIPart()
-//                                .apply(controllerData.getRequestPath()));
-//
-//                Map<String, String> queryParams = operations.getQueryParams(
-//                        registeredPath, PathOperations.getPathWithoutTheAPIPart()
-//                                .apply(controllerData.getRequestPath()));
-//
-//                id = pathParams.get("id");
-//                color = pathParams.get("color");
-//                name = queryParams.get("name");
-//                age = queryParams.get("age");
-//            }
-//        };
-//    }
+    @Test
+    void givenRegisteredPathWithRequestAndQueryParams_ShouldReturnResponseProcessedByController() {
+        String apiPrefix = ApplicationConfiguration.INSTANCE.getApiPath();
+        String requestPath = apiPrefix + "/products/1/something/blue?name=ball&age=20";
+        String body = "1,blue,ball,20";
+
+        RouterImplementation routerImplementation = new RouterImplementation();
+
+        Map<String, Controller> getRoutes = new HashMap<>();
+        getRoutes.put("products/{id}/something/{color}?name&age", getControllerWithPathAndQueryParams());
+        routerImplementation.setGetRoutes(getRoutes);
+
+        ReqContextDTO ctx = new ReqContextDTO();
+        ctx.setRequestPath(requestPath);
+        RouterResponse result = routerImplementation.get(ctx);
+
+        assertEquals(body, result.getBody());
+    }
+
+    Controller getControllerWithPathAndQueryParams() {
+        return new Controller() {
+            private String id;
+            private String color;
+            private String name;
+            private String age;
+
+
+            @Override
+            public RouterResponse getResponse() {
+                RouterResponse routerResponse = new RouterResponse();
+                routerResponse.setType(JSON);
+                routerResponse.setCode(200);
+                routerResponse.setBody(MessageFormat.format("{0},{1},{2},{3}", id, color, name, age));
+                return routerResponse;
+            }
+
+            @Override
+            public void initialize(ReqContextDTO controllerData, String registeredPath) {
+                PathCommonOperations operations = new PathCommonOperations();
+
+                Map<String, String> pathParams = operations.getPathParams(
+                        registeredPath, PathOperations.getPathWithoutTheAPIPart()
+                                .apply(controllerData.getRequestPath()));
+
+                Map<String, String> queryParams = operations.getQueryParams(
+                        registeredPath, PathOperations.getPathWithoutTheAPIPart()
+                                .apply(controllerData.getRequestPath()));
+
+                id = pathParams.get("id");
+                color = pathParams.get("color");
+                name = queryParams.get("name");
+                age = queryParams.get("age");
+            }
+        };
+    }
 
     Controller getController() {
         return new Controller() {
