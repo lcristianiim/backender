c418646cddb083d9b52e2ccb58201545c7c8c6c1
Cristian <lcristianiim@gmail.com>
2024-12-16 03:56:29 +0200

    Bringing back mongodbmodule functionality

--- mongodbmodule/src/main/java/org/backender/interactorimplementations/PersonsPersistenceImpl.java
+++ mongodbmodule/src/main/java/org/backender/interactorimplementations/PersonsPersistenceImpl.java
@@ -2,6 +2,7 @@ package org.backender.interactorimplementations;
 
 import com.mongodb.client.*;
 import org.bson.Document;
+import org.bson.types.ObjectId;
 import org.interactor.modules.datacenter.dtos.PersonDTO;
 import org.interactor.modules.datacenter.interfaces.PersonsPersistence;
 
@@ -13,33 +14,33 @@ public class PersonsPersistenceImpl implements PersonsPersistence {
     @Override
     public void save(PersonDTO person) {
 
-//        MongoDatabase database = MongoReadSingleton.INSTANCE.getDatabase();
-//
-//        MongoCollection<Document> usersCollection = database.getCollection("persons");
-//
-//        Document user = MapperUtil.toDocument(person);
-//        user.remove("_id");
-//        usersCollection.insertOne(user);
-//        ObjectId userId = user.getObjectId("_id");
+        MongoDatabase database = MongoReadSingleton.INSTANCE.getDatabase();
+
+        MongoCollection<Document> usersCollection = database.getCollection("persons");
+
+        Document user = MapperUtil.toDocument(person);
+        user.remove("_id");
+        usersCollection.insertOne(user);
+        ObjectId userId = user.getObjectId("_id");
 
 
-//        PersonsRepository.INSTANCE.save(person);
+        PersonsRepository.INSTANCE.save(person);
     }
 
     @Override
     public List<PersonDTO> findAll() {
         List<PersonDTO> result = new ArrayList<>();
-//        List<PersonDocument> personDocuments = new ArrayList<>();
-//
-//        MongoDatabase database = MongoReadSingleton.INSTANCE.getDatabase();
-//        MongoCollection<Document> personsCollection = database.getCollection("persons");
-//
-//        try (MongoCursor<Document> cursor = personsCollection.find().iterator()) {
-//            while (cursor.hasNext()) {
-//                result.add(PersonMapper.INSTANCE.toDTO(MapperUtil.fromDocument(cursor.next(), PersonDocument.class)));
-//            }
-//        }
-//
+        List<PersonDocument> personDocuments = new ArrayList<>();
+
+        MongoDatabase database = MongoReadSingleton.INSTANCE.getDatabase();
+        MongoCollection<Document> personsCollection = database.getCollection("persons");
+
+        try (MongoCursor<Document> cursor = personsCollection.find().iterator()) {
+            while (cursor.hasNext()) {
+                result.add(PersonMapper.INSTANCE.toDTO(MapperUtil.fromDocument(cursor.next(), PersonDocument.class)));
+            }
+        }
+
         return result;
     }
 }
