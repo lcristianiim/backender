93e53ef7a41ecff7fec82b8f055cd2cae6f4a63a
Cristian <lcristianiim@gmail.com>
2025-02-06 07:56:58 +0200

    Create todo's for auth-server

---
+++ interactor/src/main/java/org/interactor/controllers/users/GetActivationLinkController.java
@@ -0,0 +1,67 @@
+package org.interactor.controllers.users;
+
+import org.interactor.internals.Route;
+import org.interactor.modules.jwtauth.JWTActionResponse;
+import org.interactor.modules.jwtauth.JWTAuth;
+import org.interactor.modules.jwtauth.JWTAuthService;
+import org.interactor.modules.router.RouterService;
+import org.interactor.modules.router.dtos.Controller;
+import org.interactor.modules.router.dtos.InteractorRequest;
+import org.interactor.modules.router.dtos.InteractorResponse;
+
+import java.util.Map;
+
+import static org.interactor.configuration.RegisteredRoute.ACTIVATION_ID;
+
+public class GetActivationLinkController implements Controller {
+    JWTAuth jwtAuth = JWTAuthService.INSTANCE.getJwtAuth();
+    String activationToConfirm;
+
+    @Override
+    public InteractorResponse getResponse() {
+        JWTActionResponse registrationResponse;
+
+        try {
+            registrationResponse = jwtAuth.confirm(activationToConfirm);
+        } catch (Exception e) {
+            throw new SomethingWentWrongCallingTheAuthServiceException(e.getMessage());
+        }
+
+        return evaluateRegistrationResponseAndCreateInteractorResponse(registrationResponse);
+    }
+
+    private InteractorResponse evaluateRegistrationResponseAndCreateInteractorResponse(
+            JWTActionResponse confirmationResponse) {
+
+        if (confirmationResponse.isSuccess()) {
+            InteractorResponse response = new InteractorResponse();
+            response.setCode(200);
+            response.setBody("User has been confirmed.");
+            return response;
+        }
+
+        InteractorResponse response = new InteractorResponse();
+        response.setCode(500);
+        response.setBody(confirmationResponse.message());
+        return response;
+    }
+
+    @Override
+    public void initialize(InteractorRequest controllerData, Route registeredPath) {
+
+        Map<String, String> pathParams = RouterService.INSTANCE.getRouter()
+                .getPathParams(registeredPath.path(), controllerData.getRequestPath());
+
+            activationToConfirm = pathParams.get(ACTIVATION_ID);
+    }
+
+    public static class SomethingWentWrongCallingTheAuthServiceException extends RuntimeException {
+        public SomethingWentWrongCallingTheAuthServiceException(String message) {
+            super(message);
+        }
+    }
+
+    public void setJwtAuthForTesting(JWTAuth jwtAuth) {
+        this.jwtAuth = jwtAuth;
+    }
+}
